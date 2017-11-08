834e0a4dbcb4ae68be612a06a1ab2a1681362412
MLT <matthewlthompson@gmail.com>
2017-11-08 14:24:46 -0500

    Creates home, calendar, faq routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    render html:  "This is where the main info will be"
+  end
+
+  def calendar
+    render html:  "Calendar"
+  end
+
+  def faq
+    render html:  "This will be the FAQ"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+
+  get 'faq' => 'application#faq'
+  get 'calendar' => 'application#calendar'
 end
