cbc8afcd534b208fb458b089738f589253e89fac
Matt Thompson <NiceMLT@users.noreply.github.com>
2017-11-17 14:45:51 -0500

    Authentimication (#34)
    * Adds devise and starts configuring it
    
    * more setup
    
    * Moves public pages out to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def home
-    render "/home"
-  end
-
-  def calendar
-    render "/calendar"
-  end
-
-  def faq
-    render "/faq"
-  end
-
-  def about
-    render "/about"
-  end
-
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: { guardian: 0, player: 1, coach: 2, admin: 3 }
+
+end
